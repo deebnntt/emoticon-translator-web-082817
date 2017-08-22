416afad02900b10b992fb9c71a553eb8e9176994
github username deebnntt <github email address daniellebennettchan@gmail.com>
2017-08-22 16:36:03 -0400

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,47 @@
 # require modules here
+require "yaml"
 
-def load_library
-  # code goes here
+def load_library(path)
+  emoticons = YAML.load_file(path)
+  new_hash = {}
+  new_new_hash = {}
+
+  emoticons.each do |key, value|
+      new_hash[value[1]] = key.to_s
+      new_new_hash[value[0]] = value[1]
+  end
+  emoticon_hash = {"get_meaning" => new_hash, "get_emoticon" => new_new_hash}
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(path, english_emoticon)
+  new_hash = load_library(path)
+    new_hash["get_emoticon"].each do |key, value|
+      if key == english_emoticon
+        return value
+      elsif !new_hash["get_emoticon"].include?(english_emoticon)
+        return "Sorry, that emoticon was not found"
+      else
+    end
+  end
 end
 
-def get_english_meaning
-  # code goes here
+=begin
+this also works:
+
+def get_japanese_emoticon(path, english_emoticon)
+  new_hash = load_library(path)
+    new_hash["get_emoticon"].each do |key, value|
+    return value if key == english_emoticon
+  end
+  return "Sorry, that emoticon was not found" if !new_hash["get_emoticon"].include?(english_emoticon)
+end
+
+=end
+
+def get_english_meaning(path, japanese_emoticon)
+  new_hash = load_library(path)
+    new_hash["get_meaning"].each do |key, value|
+    return value if key == japanese_emoticon
+  end
+    return "Sorry, that emoticon was not found" if !new_hash["get_meaning"].include?(japanese_emoticon)
 end
